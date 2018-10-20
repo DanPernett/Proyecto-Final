d42ea5051f36cfb9f15e9682609b78390bea63f3
DanPernett <42698453+DanPernett@users.noreply.github.com>
2018-10-20 02:22:37 -0600

    actualizacion 2. Contactenos y nosotros
    Se suman pagina de contactenos y Nosotros completamente funcionales.
    Se modifica el inicio y se agrega contenido real y veridico cienporciento real no fake un link mega al body

---
+++ header.php
@@ -0,0 +1,28 @@
+  
+ <!DOCTYPE html>
+ <html>
+ <head>
+   <title></title>
+   <link rel="stylesheet" type="text/css" href="css/style.css">
+ </head>
+ <body>
+   <a href="index.html" id="navtitulo" >Probalo vos</a>
+  <nav>
+   <ul class="navul">
+    <li class="navulli">
+      <a href="index.html" class="nava">Inicio</a>
+    </li>
+    <li class="navulli">
+      <a href="#" class="nava">Nosotros</a>
+    </li>
+    <li class="navulli">
+      <a href="#" class="nava">¿Qué hay de nuevo?</a>
+    </li>
+    <li class="navulli">
+      <a href="#" class="nava">Contacto</a>
+    </li>
+  </ul>
+</nav>
+
+ </body>
+ </html>
